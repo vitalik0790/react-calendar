56ebda1dc337d69c9cb02d3e47dc8998177ba238
Vitalii Kovalchuk <vitalii.kovalchuk@neatbyte.solutions>
2022-09-13 00:40:34 +0300

    need to change modals

--- src/components/Day.js
+++ src/components/Day.js
@@ -3,10 +3,9 @@ import React from "react";
 const Day = ({ day, onClick }) => {
   return (
     <div
-      className={`${
-        day.value === "padding" ? "" : "h-[140px] px-3 py-3 border cursor-pointer hover:bg-green-300 duration-300"
-      } ${day.isCurrentDay ? "bg-blue-300 hover:bg-blue-300" : ""}`}
-      onClick={onClick}
+      className={`${day.value === "padding" ? "" : "h-[140px] px-3 py-3 border"} ${
+        day.isCurrentDay ? "bg-blue-300 hover:bg-blue-300" : ""
+      }`}
     >
       <div className="flex justify-between mb-2 text-black/90">
         <div className="">{day.value === "padding" ? "" : day.value}</div>
@@ -14,7 +13,7 @@ const Day = ({ day, onClick }) => {
       </div>
       {day.events &&
         day.events.map((event, index) => (
-          <div key={index} className="text-red-400 bg-gray-100">
+          <div key={index} className="text-red-400 bg-gray-100 hover:bg-gray-200 cursor-pointer mb-1 last:mb-0">
             {event.title}
           </div>
         ))}
